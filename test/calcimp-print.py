bc79af1a3b52f62b9fc1bbd1cff11598f869b2c2
Yoshinobu Ishizaki <ysnbiszk@gmail.com>
2025-10-25 12:09:18 +0900

    fix path

--- test/calcimp-print.py
+++ test/calcimp-print.py
@@ -1,6 +1,6 @@
 import calcimp
 
-f,re,im,mag = calcimp.calcimp("../sample/test.men")
+f,re,im,mag = calcimp.calcimp("sample/test.men")
 
 for fi, rei, imi,magi in zip(f,re,im,mag):
 	print(f"{fi},{rei},{imi},{magi}")
